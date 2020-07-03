0c632e52fb60fd862039d2bc17e75ee8d60909c1
seonyong-kim <introfor.blue@gmail.com>
2020-07-03 23:47:58 +0900

    added stack.py

---
+++ data_structure/Stack.py
@@ -0,0 +1,19 @@
+class stack:
+    def __init__(self):
+        self.items = []
+    def push(self, item):
+        self.items.append(item)
+    def pop(self):
+        return self.items.pop()
+    def isEmpty(self):
+        return not self.items
+    
+ 
+stk = stack()      
+print(stk)           
+print(stk.isEmpty()) 
+stk.push(1)          
+stk.push(2)          
+print(stk.pop())     
+print(stk.pop())     
+print(stk.isEmpty()) 
